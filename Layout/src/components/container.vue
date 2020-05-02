bfe147251ba13c0c6c8f7663f0dce110ff03694a
smallbrave <1060401583@qq.com>
2020-05-02 23:27:38 +0800

    input和select

---
+++ Layout/src/components/container.vue
@@ -0,0 +1,68 @@
+<template>
+<!-- container页面布局就是最常用的那种设置一个container，之后在其中使用一些html5语义化标签 -->
+<!-- 记住： header/footer的height默认值为60px-->
+<!-- 记住： aside的width默认值为300px -->
+<!-- 记住： container中的direction属性指定其中元素的排列方向，默认为垂直排列，也可设置其为 horizontal -->
+    <el-container direction="vertical">
+        <el-header>header</el-header>
+        <el-container>
+            <!-- 使用一下button组件 -->
+            <el-aside>
+                <!-- button含有type属性：用于指定按钮的基本样式，是由primary、success、info、warning、danger -->
+                <el-button type="primary">主要按钮</el-button>
+                <el-button type="success">成功按钮</el-button>
+                <el-button type="info">消息按钮</el-button>
+                <el-button type="warning">警告按钮</el-button>
+                <el-button type="danger">危险按钮</el-button>
+                
+            </el-aside>
+            <el-main>
+                <!-- 同时，还具有plain指定无背景颜色填充的button、round指定border-radius为圆角、
+                circle指定为小圆圈 ，一般用于放小图标在其中icon属性-->
+                <el-button type="primary" plain>无背景色</el-button>
+                <el-button type="primary" plain round>无背景色且为圆角</el-button>
+                <el-button type="success" plain circle icon="el-icon-check"></el-button>
+            </el-main>
+        </el-container>
+        <el-footer>
+            <!-- link文字链接： 使用<el-link></el-link> -->
+            <!-- 含有属性： type指定显示的文字类型，同上面的button；动态绑定的:underline值为boolean值
+            表示选中时是否由下划线,默认为有；disabled设置是否禁用；icon指定图标；herf指定链接 -->
+            <el-link type="primary" icon="el-icon-edit">主要按钮</el-link>
+            <el-link type="success">成功按钮</el-link>
+            <el-link type="info">消息按钮</el-link>
+            <el-link type="warning">警告按钮</el-link>
+            <el-link type="danger">危险按钮</el-link>
+            <el-link type="danger">危险按钮</el-link>
+            <el-link type="danger" :underline="false" >危险按钮</el-link>
+            <div>hhh</div>    
+        </el-footer>
+    </el-container>
+</template>
+<script>
+export default {
+    name: 'Container',
+
+    
+}
+</script>
+<style scoped>
+.el-header,
+.el-footer {
+    background-color: #fff;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
+
+.el-aside,
+.el-main {
+    line-height: 200px;
+    background-color: coral;
+    text-align: center;
+}
+
+.el-aside {
+    background-color: seashell;
+}
+</style>
---
+++ Layout/src/components/layout.vue
@@ -0,0 +1,74 @@
+<template>
+<!-- 使用row和col来实现布局需要注意的是每次都务必然row中的col的span值之和为24 -->
+
+<!-- Layout布局使用的是：通过 row 和 col 组件，并通过 col 组件的 span 属性我们就可以自由地组合布局。 -->
+<!-- <el-row></el-row>代表一行，可用其将整个页面分为几个大块，可在其上指定 :gutter="整数"属性，表示列间距 -->
+<!-- 可为<el-row></el-row>指定:type属性，值为flex，之后在其上直接使用justify属性，指定子元素的水平排版方式 -->
+<!-- 可为<el-row></el-row>指定:type属性，值为flex，之后在其上直接使用align属性，指定子元素的垂直排版方式 -->
+<!-- <el-col></el-col>代表一列，可使用:span="整数"指定列宽（一行下分为24份）;以:offset="整数"指定列偏移-->
+<!-- 可在<el-col></el-col>上指定:push/:pull属性，值为整数，表示往右移和往左移格数 -->
+  <div>
+    <el-row class="nav">
+      <el-col :offset="13" :span="11">
+        <span>登录</span><span>注册</span>
+        <el-divider direction="vertical"></el-divider>
+        <span>帮助中心</span>
+        <el-divider direction="vertical"></el-divider>
+        <span><i class="el-icon-mobile-phone"></i>下载APP</span>
+        <el-divider direction="vertical"></el-divider>
+        <span>资质证照/协议规则<i class="el-icon-arrow-down"></i></span>
+      </el-col>
+    </el-row>
+    <el-row class="banner">
+      <el-col :offset="4" :span="20">
+        <el-row class="banner-header">
+          <el-col :span="4">
+            小米有品
+          </el-col>
+          <el-col :offset="2" :span="4">
+            <span>限时抢购</span><span>企业采购</span>
+          </el-col>
+          <el-col :offset="1" :span="4">
+            <el-input class="input" prefix-icon="el-icon-search"></el-input>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-row >
+      <div class="content">
+        content
+      </div>
+    </el-row>
+    <el-row >
+      <div class="footer">footer</div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+}
+</script>
+
+<style scoped>
+.nav {
+  height: 40px;
+  line-height: 40px;
+  background-color: #ccc;
+}
+
+  .banner {
+    background-color: aqua;
+  }
+
+  .content {
+    height: 500px;
+    background-color: bisque;
+  }
+
+  .footer {
+    height: 90px;
+    background-color: brown;
+  }
+</style>
